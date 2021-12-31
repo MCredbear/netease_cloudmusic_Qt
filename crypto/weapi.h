b2d2b0b084bcaedd1b9faf6e6d6b128bbc24ae68
MCredbear <2363764471@qq.com>
2021-12-31 19:29:32 +0800

    added weapi

--- crypto/weapi.h
+++ crypto/weapi.h
@@ -13,9 +13,7 @@ public:
     QByteArray publicKey = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDgtQn2JZ3\n4ZC28NWYpAUd98iZ37BUrX/aKzmFbt7clFSs6sXqHauqKWqdtLkF2KexO40H1YTX8z2lSgBBOAxLsvakl\nV8k4cBFK9snQXE9/DDaFt6Rr7iVZMldczhC0JNgTz+SHXT6CBHuX3e9SdB1Ua44o\nncaTWz7OBGLbCiK45wIDAQAB\n-----END PUBLIC KEY-----";
     /* OpenSSL public key format:
      * -----BEGIN PUBLIC KEY-----
-     *
      * a '\n' behind every 64 words
-     *
      * -----END PUBLIC KEY-----
      */
     // learn more about OpenSSL's RSA: https://blog.csdn.net/yizhiniu_xuyw/article/details/114371606
